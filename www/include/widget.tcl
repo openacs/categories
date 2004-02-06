d3e67aed052448bbde4476bd6dd375e2cd55c222
Timo Hentschel <timo@timohentschel.de>
2004-02-06 14:00:31 +0000

    added support for require_category_p

--- www/include/widget.tcl
+++ www/include/widget.tcl
@@ -12,12 +12,12 @@ if {![info exists name]} {
     set name category_ids
 }
 
-template::multirow create trees tree_id tree_name category_id selected_p category_name indent single_select_p
+template::multirow create trees tree_id tree_name category_id selected_p category_name indent assign_single_p require_category_p
 
 template::util::list_to_lookup [category::get_mapped_categories $object_id] mapped
 
 foreach tree [category_tree::get_mapped_trees $package_id] {
-    util_unlist $tree tree_id tree_name subtree_id assign_single_p
+    util_unlist $tree tree_id tree_name subtree_id assign_single_p require_category_p
     set one_tree [list]
     foreach category [category_tree::get_tree -subtree_id $subtree_id $tree_id] {
 	util_unlist $category category_id category_name deprecated_p level
@@ -26,6 +26,7 @@ foreach tree [category_tree::get_mapped_trees $package_id] {
 	    set indent "[category::repeat_string "&nbsp;" [expr 2*$level -4]].."
 	}
 	set selected_p [info exists mapped($category_id)]
-	template::multirow append trees $tree_id $tree_name $category_id $selected_p $category_name $indent $assign_single_p
+
+	template::multirow append trees $tree_id $tree_name $category_id $selected_p $category_name $indent $assign_single_p $require_category_p
     }
 }
