de8a11bc7401545d412ffa59b21f34c40978857e
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2008-04-16 10:27:12 +0000

    Fix bug, where /categories/cadmin/object-map complains about a twice added form item "tree_id". For unknown reasons, this happens so far only in the head version, and not in the oacs-5-4 branch.

--- www/cadmin/master.tcl
+++ www/cadmin/master.tcl
@@ -7,16 +7,17 @@ if { ![info exists change_locale] } {
 }
 
 if {![exists_and_not_null locale]} {
-    set locale [ad_parameter DefaultLocale acs-lang "en_US"]
+    #set locale [ad_parameter DefaultLocale acs-lang "en_US"]
     set locale [ad_conn locale]
 }
 
 set languages [lang::system::get_locale_options]
-
 set vars_to_export_list {tree_id category_id}
 
 set set_id [ad_conn form]
-set varname_list [ad_ns_set_keys -exclude {locale form:mode form:id __confirmed_p __refreshing_p formbutton:ok} $set_id]
+set varname_list [ad_ns_set_keys -exclude {
+  tree_id category_id locale form:mode form:id __confirmed_p __refreshing_p formbutton:ok
+} $set_id]
 
 foreach name $varname_list {
     set $name [ns_set get $set_id $name]
