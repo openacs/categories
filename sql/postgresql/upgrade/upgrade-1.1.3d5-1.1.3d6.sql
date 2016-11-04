8c24ed95ea463dd0e759d622ce746977ed41dddf
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2016-11-04 11:09:25 +0000

    - add missing upgrade script

---
+++ sql/postgresql/upgrade/upgrade-1.1.3d5-1.1.3d6.sql
@@ -0,0 +1,2 @@
+DROP FUNCTION IF EXISTS category__new(integer, integer, character varying, character varying, character varying, integer, character, timestamp with time zone, integer, character varying);
+
